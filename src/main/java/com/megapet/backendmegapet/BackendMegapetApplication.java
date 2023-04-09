8ea4a169151ee64be5af4ee30aae661abd041f5c
2CTO <u201521691@upc.edu.pe>
2023-04-08 23:06:17 -0500

    initial files to init working

---
+++ src/main/java/com/megapet/backendmegapet/BackendMegapetApplication.java
@@ -0,0 +1,13 @@
+package com.megapet.backendmegapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMegapetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMegapetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megapet/backendmegapet/BackendMegapetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megapet.backendmegapet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMegapetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
